107ea570afe5a0342a50229add7e05e0aefc94bf
LDeepakReddy <l.deepakreddy.com>
2022-04-06 00:17:43 +0530

    ADDED:Functionality to get the cheapest hotel on given days

--- src/main/java/com/blz/hotelreservationsystem/HotelReservationSystem.java
+++ src/main/java/com/blz/hotelreservationsystem/HotelReservationSystem.java
@@ -1,15 +1,22 @@
 package com.blz.hotelreservationsystem;
 
+import java.security.KeyStore;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class HotelReservationSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to the Hotel Reservation System Program");
-
     }
 
-    Map<String, Hotel> hotelMap = new HashMap<String, Hotel>();
+    Map<String, Hotel> hotelMap = new HashMap<>();
 
     public boolean addHotel(String hotelNumber, String hotelName, int weekDayRates, int weekEndRates) {
         Hotel hotel = new Hotel();
@@ -31,6 +38,16 @@ public class HotelReservationSystem {
             System.out.println(" \n Hotel Name : " + value.hotelName + " \nRates On weekdays : " + value.weekDayRates +
                     "\n Rates on Weekwnds : " + value.weekEndRates);
         }
+    }
+
+    public Hotel cheapestHotel(LocalDate startDate, LocalDate endDate) {
+        long numOfDaysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+        Optional<Hotel> hotelStream = hotelMap.values().stream().limit(numOfDaysBetween).min(Comparator.comparing(Hotel::getWeekDayRates));
+        System.out.println(LocalDate.of(2020, 03, 16).getDayOfWeek());
+        System.out.println(LocalDate.of(2020, 03, 17).getDayOfWeek());
+
+        return hotelStream.get();
+
     }
 
 
--- src/test/java/com/blz/hotelreservationsystem/HotelReservationSystemTest.java
+++ src/test/java/com/blz/hotelreservationsystem/HotelReservationSystemTest.java
@@ -3,17 +3,36 @@ package com.blz.hotelreservationsystem;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class HotelReservationSystemTest {
+    HotelReservationSystem hotels = new HotelReservationSystem();
+
     @Test
     public void givenHotel_whenDetailsEntered_ForRegularCustomers_ShouldReturnTrue() {
-        HotelReservationSystem hotels = new HotelReservationSystem();
-        boolean hotel_1 = hotels.addHotel("Hotel-1", "LakeWood", 110, 90);
-        Assert.assertEquals(true, hotel_1);
-        boolean hotel_2 = hotels.addHotel("Hotel-2", "BridgeWood", 160, 60);
-        Assert.assertEquals(true, hotel_2);
-        boolean hotel_3 = hotels.addHotel("Hotel-3", "RidgeWood", 220, 150);
-        Assert.assertEquals(true, hotel_3);
+
+        boolean hotel1 = hotels.addHotel("Hotel-1", "LakeWood", 110, 90);
+        Assert.assertEquals(true, hotel1);
+        boolean hotel2 = hotels.addHotel("Hotel-2", "BridgeWood", 160, 60);
+        Assert.assertEquals(true, hotel2);
+        boolean hotel3 = hotels.addHotel("Hotel-3", "RidgeWood", 220, 150);
+        Assert.assertEquals(true, hotel3);
         hotels.displayHotel();
+    }
+
+    @Test
+    public void givenRangeOfDates_WithRatesOnWeekdaysAndWeekends_ShouldReturnCheapestHotel() {
+
+        hotels.addHotel("Hotel-1", "LakeWood", 110, 90);
+        hotels.addHotel("Hotel-2", "BridgeWood", 160, 60);
+        hotels.addHotel("Hotel-3", "RidgeWood", 220, 150);
+
+        LocalDate startDate = LocalDate.of(2020, 03, 16);
+        LocalDate endDate = LocalDate.of(2020, 03, 17);
+        Hotel hotel = hotels.cheapestHotel(startDate, endDate);
+        Assert.assertEquals("LakeWood", hotel.getHotelName());
+        System.out.println("Cheapest hotel is " + hotel.getHotelName());
+
     }
 
 }
